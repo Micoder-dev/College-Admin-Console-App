e53da55d8ab2f378977311b8f160d6126bc1c27b
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-07 22:48:48 +0530

    select image from gallery & display it below for preview

--- app/src/main/java/com/micoder/collegeapp/MainActivity.java
+++ app/src/main/java/com/micoder/collegeapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.micoder.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    CardView uploadNotice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uploadNotice=findViewById(R.id.addNotice);
+
+        uploadNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.addNotice:
+                startActivity(new Intent(MainActivity.this,UploadNotice.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/micoder/collegeapp/UploadNotice.java
+++ app/src/main/java/com/micoder/collegeapp/UploadNotice.java
@@ -1,14 +1,58 @@
 package com.micoder.collegeapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UploadNotice extends AppCompatActivity {
+
+    private CardView addImage;
+    private final int REQ=1;
+    private Bitmap bitmap;
+    private ImageView noticeImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_notice);
+
+        addImage=findViewById(R.id.addImage);
+        noticeImageView=findViewById(R.id.noticeImageView);
+
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent pickImage=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickImage,REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQ && resultCode==RESULT_OK){
+            Uri uri=data.getData();
+            try {
+                bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            noticeImageView.setImageBitmap(bitmap);
+        }
     }
 }
