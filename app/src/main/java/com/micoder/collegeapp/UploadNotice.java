cb393f2bfdaacbef07fdf2aebc5f7fa393913ef7
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-09 23:34:29 +0530

    upload teachers data in firebase

--- app/src/main/java/com/micoder/collegeapp/UploadNotice.java
+++ app/src/main/java/com/micoder/collegeapp/UploadNotice.java
@@ -40,7 +40,7 @@ public class UploadNotice extends AppCompatActivity {
     private ImageView noticeImageView;
     private EditText noticeTitle;
     private Button uploadNoticeBtn;
-    private DatabaseReference reference;
+    private DatabaseReference reference,dbRef;
     private StorageReference storageReference;
     String downloadUrl = "";
     private ProgressDialog pd;
@@ -116,8 +116,8 @@ public class UploadNotice extends AppCompatActivity {
     }
 
     private void uploadData() {
-        reference=reference.child("Notice");
-        final String uniqueKey=reference.push().getKey();
+        dbRef=reference.child("Notice");
+        final String uniqueKey=dbRef.push().getKey();
 
         String title=noticeTitle.getText().toString();
 
@@ -131,7 +131,7 @@ public class UploadNotice extends AppCompatActivity {
 
         NoticeData noticeData=new NoticeData(title,downloadUrl,date,time,uniqueKey);
 
-        reference.child(uniqueKey).setValue(noticeData).addOnSuccessListener(new OnSuccessListener<Void>() {
+        dbRef.child(uniqueKey).setValue(noticeData).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
                 pd.dismiss();
