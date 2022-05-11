2ec4d16bf74fda4bfeb8223fa3da5aa7901c82a9
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-11 15:22:51 +0530

    setup layout for delete notice & newsfeed_item_layout

---
+++ app/src/main/java/com/micoder/collegeapp/notice/DeleteNoticeActivity.java
@@ -0,0 +1,16 @@
+package com.micoder.collegeapp.notice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.micoder.collegeapp.R;
+
+public class DeleteNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_notice);
+    }
+}
