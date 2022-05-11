99f4bd5d2bc1b860f4a8d7d81a0525c241e9b0e3
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-11 20:17:55 +0530

    alert dialog added in delete notice

--- app/src/main/java/com/micoder/collegeapp/notice/NoticeAdapter.java
+++ app/src/main/java/com/micoder/collegeapp/notice/NoticeAdapter.java
@@ -1,6 +1,7 @@
 package com.micoder.collegeapp.notice;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -56,20 +58,47 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeView
         holder.deleteNotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("Notice");
-                reference.child(currentItem.getKey()).removeValue()
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                AlertDialog.Builder builder=new AlertDialog.Builder(context);
+                builder.setMessage("Are you sure want to delete this notice?");
+                builder.setCancelable(true);
+                builder.setPositiveButton(
+                        "OK",
+                        new DialogInterface.OnClickListener() {
                             @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            public void onClick(DialogInterface dialog, int which) {
+                                DatabaseReference reference= FirebaseDatabase.getInstance().getReference().child("Notice");
+                                reference.child(currentItem.getKey()).removeValue()
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                notifyItemRemoved(position);
                             }
                         });
-                notifyItemRemoved(position);
+                                builder.setNegativeButton(
+                                        "Cancel", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                dialogInterface.cancel();
+                                            }
+                                        });
+                                AlertDialog dialog=null;
+                try {
+                    dialog= builder.create();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (dialog!=null)
+                     dialog.show();
+
             }
         });
     }
